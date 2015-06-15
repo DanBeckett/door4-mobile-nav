47b75e8bf23e94c660210934199880c3830f286a
Dan Beckett <dan.beckett@door4.com>
2015-06-15 18:40:16 +0100

    updated to use jQuery to prepend the nav to body, rather than wp_head call which drops into <head> tags on newer versions of WordPress

--- door4-mobile-nav.php
+++ door4-mobile-nav.php
@@ -3,7 +3,7 @@
  * Plugin Name: Door4 Mobile Nav
  * Plugin URI: http://www.door4.com
  * Description: A sliding menu to be included on Door4 sites for Mobile Versions.
- * Version: 0.7.4
+ * Version: 0.9
  * Author: Dan Beckett
  * Author URI: http://www.door4.com
  * License: GPL2
@@ -61,6 +61,11 @@ function add_sub_menu($item_id, $section_title, $menu_array) {
 	return $sub_output;	
 };
 
+/*  Deprecated in 0.9 as tapping directly into wp_head was causing
+    incorrect load into <head> section.
+    
+    Replaced with include_door4_mobile_menu_js, performing same function
+    but adding to body with jQuery
 if(!function_exists('include_door4_mobile_menu')) {
 
 	function include_door4_mobile_menu() {
@@ -105,8 +110,12 @@ if(!function_exists('include_door4_mobile_menu')) {
 
 	add_action( 'wp_head', 'include_door4_mobile_menu' );
 };
+*/
 
 
+/*  Deprecated in version 0.9 and rolled into one function 
+	- include_door4_mobile_menu_js
+	
 if(!function_exists('include_door4_mobile_menu_close')) {
 
 	function include_door4_mobile_menu_close() {
@@ -116,6 +125,59 @@ if(!function_exists('include_door4_mobile_menu_close')) {
 
 	add_action( 'wp_footer', 'include_door4_mobile_menu_close' );
 };
+*/
 
- 
+if(!function_exists('include_door4_mobile_menu_js')) {
+
+	function include_door4_mobile_menu_js() {
+
+		global $menu_location_name;
+		global $menu_array;
+		
+		$output = '<a href=\"javascript:void(0);\" class=\"door4-mobile-nav-toggle\"><i class=\"icon-reorder\"></i></a><div class=\"door4-mobile-nav\"><ul class=\"mobile-nav-list\"><li class=\"section-title\"><h4 class=\"door4-mobile-nav-title\">Navigate</h4></li>';
+		
+		$menu_locations = get_nav_menu_locations();
+		$menu_id = $menu_locations[$menu_location_name];		
+		
+		$menu_items = wp_get_nav_menu_items($menu_id);
+	
+		if(is_array($menu_items)) {
+	
+			foreach($menu_items as $item) {
+				$nav_parent = $item->menu_item_parent;
+				$menu_array[$nav_parent][] = $item;
+			};
+			
+			foreach($menu_array[0] as $top_level_item) {
+				$item_id = $top_level_item->ID;
+				$has_sub = 'blank';
+				if($menu_array[$item_id]) $has_sub = 'caret-left';
+				$output.= '<li class=\"mobile-nav-item\"><a class=\"mobile-nav-link hasi-'.$has_sub.'\" href=\"'.$top_level_item->url.'\">';
+				$output.= '<i class=\"icon-'.$has_sub.'\"></i><span>'.$top_level_item->title.'</span></a>';
+				if($menu_array[$item_id]) {
+					$output.= add_sub_menu($item_id, $top_level_item->title, $menu_array);
+				}
+				$output.= '</li>';
+			}
+		
+		}
+		
+		$output.= '</ul></div>';
+		$output.= '<div class=\"door4-mobile-nav-push\"><div class=\"door4-mobile-nav-overlay\"></div><div class=\"door4-mobile-nav-push-inner\">';
+		
+		//$output = '<div class=\"buzzin\" style=\"background-color: red; height: 200px; width: 200px;\"></div>';
+		
+		$newoutput = "<script type=\"text/javascript\">
+		jQuery(document).ready(function(){
+			jQuery('body').prepend('".$output."');
+			jQuery('body').append('</div></div>');
+		});</script>";
+		
+		echo $newoutput;
+		
+	};
+
+	add_action( 'wp_head', 'include_door4_mobile_menu_js' );
+};
+
 ?>
